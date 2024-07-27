f7dd9cb66b27f14154dfdade5369b0c0885a291a
Satyajeet Singh <satyajeet1152000@gmail.com>
2024-07-28 00:09:45 +0530

    infobar done

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import InfoBar from "./InfoBar";
 
 const Dashboard = () => {
     const user = { name: "Satyajeet Singh" };
     return (
         <div className=" w-full h-full">
             <Header name={user.name} />
+            <InfoBar />
         </div>
     );
 };
