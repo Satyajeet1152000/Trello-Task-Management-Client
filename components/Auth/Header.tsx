daaa93f1c0ec1815c58e6908e687bfebbecc63d9
Satyajeet Singh <satyajeet1152000@gmail.com>
2024-08-10 21:10:12 +0530

    headind update from webflo to trello

--- components/Auth/Header.tsx
+++ components/Auth/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     return (
         <div className=" w-full flex flex-col gap-y-4 items-center justify-center">
             <h1 className={cn("text-5xl font-semibold", font.className)}>
-                Welcome to <span className="text-[#4534AC]">Workflo</span>!
+                Welcome to <span className="text-[#4534AC]">Trello</span>!
             </h1>
         </div>
     );
